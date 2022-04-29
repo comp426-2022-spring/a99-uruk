02482aa16abc848f77908c5d1713878df3aabcfb
cpliebe <69869906+cpliebe@users.noreply.github.com>
2022-04-29 16:32:55 -0400

    Update script.js

--- public/examplepage/script.js
+++ public/examplepage/script.js
@@ -1,6 +1,6 @@
-const showDataButtonUS = document.getElementById('showDataButtonUS')
-showDataButtonUS.addEventListener("click",fetchUsData)
-function fetchUsData() {
+const showDataButton = document.getElementById('showDataButton')
+showDataButton.addEventListener("click",fetchData)
+function fetchData() {
     fetch('https://api.covidtracking.com/v2/us/daily.json')
     .then(function(response){
         return response.json();
@@ -51,87 +51,40 @@ function fetchUsData() {
         document.getElementById("change_from_prior_day_deaths").innerHTML = result.data[0].outcomes.death.total.calculated.change_from_prior_day;
         document.getElementById("seven_day_change_percent_deaths").innerHTML = result.data[0].outcomes.death.total.calculated.seven_day_change_percent;
         document.getElementById("seven_day_average_deaths").innerHTML = result.data[0].outcomes.hospitalized.on_ventilator.currently.calculated.seven_day_average;
+
+
+         // Chart One
+        let hospitalized = result.data[0].outcomes.hospitalized.currently.value;
+        let icu = result.data[0].outcomes.hospitalized.in_icu.currently.value;
+        let vent = result.data[0].outcomes.hospitalized.on_ventilator.currently.value;
+        let dead = result.data[0].outcomes.death.total.calculated.change_from_prior_day;
+
+         let labels1 = ["Hospitalized", "In ICU", "On Ventilator", "Deaths"];
+        let data1 = [hospitalized, icu, vent, dead];
+
+         let colors1 = ['#0000FF', '#7FFF00', '#FF7F50', '#6495ED',];
+ 
+         let myChart1 = document.getElementById("myChart").getContext('2d');
+         let chart1 = new Chart(myChart1, {
+             type: 'bar',
+             data: {
+                 labels: labels1, 
+                 datasets: [ {
+                     data: data1, 
+                     backgroundColor: colors1
+                 }]
+             },
+             options: {
+                 title: {
+                     text: "CURRENT SEVERITY CHART",
+                     display: true
+                 },
+                 legend: {
+                     display: false
+                 }
+             }
+         });
         
-        let labels1 = ["Cases", , "US Population"];
-        let data1 = [result.data[0].cases.total.calculated.population_percent, 100 - result.data[0].cases.total.calculated.population_percent];
-        let colors1 = ['#49A9EA', '#36CAAB'];
-
-        let myChart1 = document.getElementById("myChart").getContext('2d');
-        let chart1 = new Chart(myChart1, {
-            type: 'doughnut',
-            data: {
-                labels: labels1, 
-                datasets: [ {
-                    data: data1, 
-                    backgroundColor: colors1
-                }]
-            },
-            options: {
-                title: {
-                    text: "US COVID DATA",
-                    display: true
-                }
-            }
-        });
-
-
-    })
 }
-
-var input;
-function getStateName() {
-    input = document.getElementById("userInput").value;
-}
-const showDataButtonST = document.getElementById('showDataButtonST')
-showDataButtonST.addEventListener("click",fetchStData)
-function fetchStData() {
-    input = document.getElementById("userInput").value
-    fetch('https://api.covidtracking.com/v2/states/' + input + '/daily.json')
-    .then(function(response){
-        return response.json();
-    }) 
-    .then(function(result) {
-        console.log(result);
-
-
-        // CASES
-        document.getElementById("stateName").innerHTML = result.data[0].state;
-        document.getElementById("dateUpdated_hs").innerHTML = result.data[0].date;
-        document.getElementById("totalCases_hs").innerHTML = result.data[0].cases.total.value;
-        document.getElementById("population_percent_hs").innerHTML = result.data[0].cases.total.calculated.population_percent;
-        document.getElementById("change_from_prior_day_hs").innerHTML = result.data[0].cases.total.calculated.change_from_prior_day;
-        document.getElementById("seven_day_change_percent_hs").innerHTML = result.data[0].cases.total.calculated.seven_day_change_percent;
-        
-        // TESTING 
-        document.getElementById("totalTests_hs").innerHTML = result.data[0].tests.pcr.total.value;
-        document.getElementById("population_percent_hs_tests").innerHTML = result.data[0].tests.pcr.total.calculated.population_percent;
-        document.getElementById("change_from_prior_day_hs_tests").innerHTML = result.data[0].tests.pcr.total.calculated.change_from_prior_day;
-        document.getElementById("seven_day_change_percent_hs_tests").innerHTML = result.data[0].tests.pcr.total.calculated.seven_day_change_percent;
-
-        // OUTCOMES  
-
-        // ICU
-        document.getElementById("totalICU_hs").innerHTML = result.data[0].outcomes.hospitalized.in_icu.currently.value;
-        document.getElementById("population_percent_hs_icu").innerHTML = result.data[0].outcomes.hospitalized.in_icu.currently.calculated.population_percent;
-        document.getElementById("change_from_prior_day_hs_icu").innerHTML = result.data[0].outcomes.hospitalized.in_icu.currently.calculated.change_from_prior_day;
-        document.getElementById("seven_day_change_percent_hs_icu").innerHTML = result.data[0].outcomes.hospitalized.in_icu.currently.calculated.seven_day_change_percent;
-        document.getElementById("seven_day_average_hs_icu").innerHTML = result.data[0].outcomes.hospitalized.in_icu.currently.calculated.seven_day_average;
-
-        // VENTILATOR
-        document.getElementById("totalVent_hs").innerHTML = (result.data[0].outcomes.hospitalized.on_ventilator.currently.value + "");
-        document.getElementById("population_percent_hs_vent").innerHTML = (result.data[0].outcomes.hospitalized.on_ventilator.currently.calculated.population_percent+ "");
-        document.getElementById("change_from_prior_day_hs_vent").innerHTML = (result.data[0].outcomes.hospitalized.on_ventilator.currently.calculated.change_from_prior_day+ "");
-        document.getElementById("seven_day_change_percent_hs_vent").innerHTML = (result.data[0].outcomes.hospitalized.on_ventilator.currently.calculated.seven_day_change_percent+ "");
-        document.getElementById("seven_day_average_hs_vent").innerHTML = (result.data[0].outcomes.hospitalized.on_ventilator.currently.calculated.seven_day_average+ "");
-
-
-
-        // DEATHS
-        document.getElementById("totalDeaths_hs").innerHTML = (result.data[0].outcomes.death.total.value + "");
-        document.getElementById("population_percent_hs_death").innerHTML = (result.data[0].outcomes.death.total.calculated.population_percent+ "");
-        document.getElementById("change_from_prior_day_hs_death").innerHTML = (result.data[0].outcomes.death.total.calculated.change_from_prior_day+ "");
-        document.getElementById("seven_day_change_percent_hs_death").innerHTML = (result.data[0].outcomes.death.total.calculated.seven_day_change_percent+ "");
-        document.getElementById("seven_day_average_hs_death").innerHTML = (result.data[0].outcomes.death.total.calculated.seven_day_average+ "");
-
-    })
+    )}
 }
