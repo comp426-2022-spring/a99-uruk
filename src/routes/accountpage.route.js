af4b0123ca8438311613bd6c0e2cf945fe09e4f6
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-05-02 03:04:50 -0400

    comment change

--- src/routes/accountpage.route.js
+++ src/routes/accountpage.route.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 
-// Serves index.html for coviddata page
+// Serves index.html for account page
 router.route('/app/accountpage/').get(function (req, res, next) {
     res.sendFile(path.join(__dirname.slice(0,-11), 'public/accountpage/index.html'));
 })
 
-// Returns style.css for coviddata page
+// Returns style.css for account page
 router.route('/app/accountpage/style.css').get(function (req, res, next) {
     res.sendFile(path.join(__dirname.slice(0,-11), 'public/accountpage/style.css'));
 })
 
-// Returns script.js for coviddata page
+// Returns script.js for account page
 router.route('/app/accountpage/script.js').get(function (req, res, next) {
     res.sendFile(path.join(__dirname.slice(0,-11), 'public/accountpage/script.js'));
 })
